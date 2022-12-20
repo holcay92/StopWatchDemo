f4d9a5b01fa3c379d93b19e227479192c1280b78
halil.olcay <holcay92@gmail.com>
2022-12-21 01:15:57 +0300

    service implemented and clock is displaying time correctly

--- app/src/main/java/com/example/stopwatchdemo/MainActivity.kt
+++ app/src/main/java/com/example/stopwatchdemo/MainActivity.kt
@@ -1,25 +1,35 @@
 package com.example.stopwatchdemo
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.stopwatchdemo.databinding.ActivityMainBinding
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var isStarted = false
+    private lateinit var serviceIntent: Intent
+    private var time = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnStart.setOnClickListener {
-           startOrStop()
+            startOrStop()
         }
         binding.btnReset.setOnClickListener {
             reset()
         }
+        serviceIntent = Intent(applicationContext, StopWatchService::class.java)
+        registerReceiver(updateTime, IntentFilter(StopWatchService.UPDATED_TIME))
 
     }
+
     private fun startOrStop() {
         if (isStarted) {
             stop()
@@ -27,15 +37,42 @@ class MainActivity : AppCompatActivity() {
             start()
         }
     }
-    private fun start(){
+
+    private fun start() {
+        serviceIntent.putExtra(StopWatchService.CURRENT_TIME, time)
+        startService(serviceIntent)
         binding.btnStart.text = "Stop"
         isStarted = true
     }
-    private fun stop(){
+
+    private fun stop() {
+        stopService(serviceIntent)
         binding.btnStart.text = "Start"
         isStarted = false
     }
-    private fun reset(){
 
+    private fun reset() {
+        stop()
+        time = 0.0
+        binding.tvTime.text = getFormattedTime(time)
+    }
+
+
+    private val updateTime: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent) {
+            time = intent.getDoubleExtra(StopWatchService.CURRENT_TIME, 0.0)
+            binding.tvTime.text = getFormattedTime(time)
+
+
+        }
+    }
+
+    private fun getFormattedTime(time: Double): String {
+        val timeInt = time.roundToInt()
+        val hours = timeInt % 86400 / 3600
+        val minutes = timeInt % 86400 % 3600 / 60
+        val seconds = timeInt % 86400 % 3600 % 60
+        val miliSeconds = (time * 1000).roundToInt() % 1000
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds)
     }
 }
--- app/src/main/java/com/example/stopwatchdemo/StopWatchService.kt
+++ app/src/main/java/com/example/stopwatchdemo/StopWatchService.kt
@@ -4,16 +4,36 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import java.util.Timer
+import java.util.TimerTask
 
-class StopWatchService: Service() {
-    override fun onBind(p0: Intent?): IBinder?= null
+class StopWatchService : Service() {
+    override fun onBind(p0: Intent?): IBinder? = null
     private val timer = Timer()
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        return super.onStartCommand(intent, flags, startId)
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        val time = intent.getDoubleExtra(CURRENT_TIME, 0.0)
+        timer.scheduleAtFixedRate(StopWatchTimerTask(time), 0, 1000)
+        return START_NOT_STICKY
     }
 
     override fun onDestroy() {
+        timer.cancel()
         super.onDestroy()
     }
+
+    companion object {
+        const val CURRENT_TIME = "current time"
+        const val UPDATED_TIME = "updated time"
+    }
+
+    private inner class StopWatchTimerTask(private var time: Double) : TimerTask() {
+        override fun run() {
+            val intent = Intent(UPDATED_TIME)
+            time++
+            intent.putExtra(CURRENT_TIME, time)
+            sendBroadcast(intent)
+        }
+    }
 }
+
+
